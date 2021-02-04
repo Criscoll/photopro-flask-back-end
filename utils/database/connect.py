2f7e57b4d114b8e6c122baa29a688a52deb63a0e
Criscoll <cristiand2021@gmail.com>
2021-02-04 12:30:07 +1100

    updated host name

--- utils/database/connect.py
+++ utils/database/connect.py
@@ -1,8 +1,9 @@
+import os
 import psycopg2
 
 
 def get_conn_and_cur():
-    host = "postgres://iglwjbwrhnmhev:4f289a07ddcc5452f9a785f89a769c3817661465a15e46e733f8c2bba78518fe@ec2-52-72-162-207.compute-1.amazonaws.com:5432/d8ek2he65l0bdh"
+    host = os.environ["DATABASE_URL"]
     database_user = "cristiand2021@gmail.com"
     database_password = "Mella*Comy12"
     database_name = "DATABASE_URL"
