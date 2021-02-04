f3d78f5aa53b3794601157fc799c64988c2ff71d
Criscoll <cristiand2021@gmail.com>
2021-02-04 12:35:12 +1100

    fixing connection

--- utils/database/connect.py
+++ utils/database/connect.py
@@ -3,16 +3,8 @@ import psycopg2
 
 
 def get_conn_and_cur():
-    host = os.environ["DATABASE_URL"]
-    database_user = "cristiand2021@gmail.com"
-    database_password = "Mella*Comy12"
-    database_name = "DATABASE_URL"
+    DATABASE_URL = os.environ["DATABASE_URL"]
 
-    conn = psycopg2.connect(
-        user=database_user,
-        password=database_password,
-        host=host,
-        database=database_name,
-    )
+    conn = psycopg2.connect(DATABASE_URL, sslmode="require")
     cur = conn.cursor()
     return conn, cur
